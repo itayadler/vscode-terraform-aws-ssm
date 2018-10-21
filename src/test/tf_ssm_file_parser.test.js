7d368fbe3e9185902d26076745d06a44c0c5d70a
Itay Adler <itayadler@gmail.com>
2018-10-22 01:05:48 +0300

    parsed example successfully with passing test

--- src/test/tf_ssm_file_parser.test.js
+++ src/test/tf_ssm_file_parser.test.js
@@ -1,9 +1,8 @@
 require('ts-node').register();
-// const assert = require('assert');
+const assert = require('assert');
 const { readFileSync } = require('fs');
 const { resolve } = require('path');
-const { Ast, ParseError } = require('../ast');
-const { parseHcl } = require('../hcl-hil/hcl-hil-wrapper');
+const { extractSSMKeysFromFile } = require('../tf_ssm_file_parser');
 
 describe("tf_ssm_file_parser", function () {
 
@@ -12,7 +11,15 @@ describe("tf_ssm_file_parser", function () {
   })
 
   it("should parse ssm keys", function () {
-    const [ast, error] = parseHcl(this.exampleFile);
-    console.log(ast.Node.Items[0].Keys);
+    const result = extractSSMKeysFromFile(this.exampleFile);
+    assert.equal(result.length, 2);
+    assert.equal(result[0].Path, '"/${data.terraform_remote_state.vpc.env}/${data.terraform_remote_state.vpc.region}/some-service/key_one"');
+    assert.equal(result[0].Line, 2);
+    assert.equal(result[0].Column, 10);
+    assert.equal(result[0].Offset, 46);
+    assert.equal(result[1].Path, '"/${data.terraform_remote_state.vpc.env}/${data.terraform_remote_state.vpc.region}/some-service/key_two"');
+    assert.equal(result[1].Line, 6);
+    assert.equal(result[1].Column, 10);
+    assert.equal(result[1].Offset, 200);
   });
 });
